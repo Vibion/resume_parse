ff0812e32e79bf931d14b2e5405039451a99eaff
Vibion <1692557820@qq.com>
2023-06-06 20:33:07 +0800

    修改多页简历转图片丢失bug

--- code/file_exet.py
+++ code/file_exet.py
@@ -10,5 +10,5 @@ for i in range(1,101):
     # convert(f"/root/autodl-tmp/resume_parse/data/{i}.docx", f"/root/autodl-tmp/resume_parse/data_pdf/{i}.pdf")
     
     pages = convert_from_path(f"/root/autodl-tmp/resume_parse/data_pdf/{i}.pdf")
-    for page in pages:
-        page.save(f"/root/autodl-tmp/resume_parse/data_png/{i}.png", 'PNG')
+    for index , page in enumerate(pages):
+        page.save(f"/root/autodl-tmp/resume_parse/data_png/{i}_{index}.png", 'PNG')
